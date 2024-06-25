3684c3d1b2d9f6bfd05220b0845d79ae6838f188
mauro-gaeta <g_mauro@msn.com>
2024-06-25 17:17:04 +0200

    Create style.css

---
+++ style.css
@@ -0,0 +1,57 @@
+body {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+}
+
+header {
+    background-color: #4CAF50;
+    color: white;
+    padding: 1rem;
+    text-align: center;
+}
+
+nav ul {
+    list-style-type: none;
+    padding: 0;
+}
+
+nav ul li {
+    display: inline;
+    margin: 0 1rem;
+}
+
+nav ul li a {
+    color: white;
+    text-decoration: none;
+}
+
+.carousel {
+    display: flex;
+    overflow: hidden;
+    width: 100%;
+    height: 300px;
+}
+
+.carousel img {
+    width: 100%;
+    height: auto;
+}
+
+main {
+    padding: 1rem;
+}
+
+#documentList {
+    list-style-type: none;
+    padding: 0;
+}
+
+#documentList li {
+    margin: 0.5rem 0;
+}
+
+#documentList li a {
+    text-decoration: none;
+    color: #333;
+}
